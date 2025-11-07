6ecdf53fde2935e0184f7e34e92cb962467a636a
root <root@b24.local>
2025-11-07 06:55:38 -0500

    add usertype

---
+++ local/php_interface/include/events.php
@@ -0,0 +1,15 @@
+<?php
+
+use Bitrix\Main\EventManager;
+
+$eventManager = EventManager::getInstance();
+
+// пользовательский тип для свойства инфоблока
+$eventManager->AddEventHandler(
+    'iblock',
+    'OnIBlockPropertyBuildList',
+    [
+        'Otus\UserTypes\Booking', // класс обработчик пользовательского типа свойства 
+        'GetUserTypeDescription'
+    ]
+);
